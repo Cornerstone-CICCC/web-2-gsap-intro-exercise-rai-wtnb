f2b5aad9c95f5c17cc3d97cececa9d9d2c0e7632
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-12-09 20:26:53 -0800

    chore: finish

--- app.js
+++ app.js
@@ -1,1 +1,88 @@
-// PUT YOUR CODE HERE
+const tl = gsap.timeline({ defaults: { duration: 0.5 }, delay: 1 });
+
+// Step 1
+tl.from(".red", { width: 0, height: 0 })
+  .from(".green", { width: 0, height: 0 })
+  .from(".blue", { width: 0, height: 0 })
+  .from(".yellow", { width: 0, height: 0 })
+
+  // Step 2
+  .to(".red", {
+    x: "100vw",
+    xPercent: -100,
+    borderRadius: "50%",
+    backgroundColor: "green",
+    duration: 1,
+  })
+  .to(
+    ".green",
+    {
+      y: "100vh",
+      yPercent: -100,
+      borderRadius: "50%",
+      backgroundColor: "blue",
+      duration: 1,
+    },
+    "<"
+  )
+  .to(
+    ".blue",
+    {
+      x: "-100vw",
+      xPercent: 100,
+      borderRadius: "50%",
+      backgroundColor: "yellow",
+      duration: 1,
+    },
+    "<"
+  )
+  .to(
+    ".yellow",
+    {
+      y: "-100vh",
+      yPercent: 100,
+      borderRadius: "50%",
+      backgroundColor: "red",
+      duration: 1,
+    },
+    "<"
+  )
+
+  // Step 3
+  .to(".red", {
+    x: "100vw",
+    xPercent: 100,
+    borderRadius: "50%",
+    backgroundColor: "green",
+  })
+  .to(
+    ".green",
+    {
+      x: "100%",
+      borderRadius: "50%",
+      backgroundColor: "blue",
+      duration: 0.25,
+    },
+    "<"
+  )
+  .to(
+    ".blue",
+    {
+      x: "-100vw",
+      xPercent: -100,
+      borderRadius: "50%",
+      backgroundColor: "yellow",
+    },
+    "<"
+  )
+  .to(
+    ".yellow",
+    {
+      x: "-100vw",
+      xPercent: -100,
+      borderRadius: "50%",
+      backgroundColor: "red",
+      duration: 4,
+    },
+    "<"
+  );
